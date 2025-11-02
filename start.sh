8abb399ab2d2d77b19fc1645fa73601625f118de
S.H. <gamelinks007@gmail.com>
2025-11-03 03:35:31 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
 PORT="${PORT:-8080}"
 
 mkdir -p /workspace /data
 
-chown -R 1000:1000 /workspace /data || true
-chmod 775 /workspace /data || true
-
-exec gosu coder:coder code-server \
+exec code-server \
   --user-data-dir /data \
   --bind-addr 0.0.0.0:"$PORT"
